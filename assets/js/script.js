b35bddecfe68d13c8338ca4edeec66ae52221e97
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-19 09:04:51 +0200

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function() {
+    let layer = document.querySelector('.price-image');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.2) / 7) + 'px,' + ((event.clientY * 0.2) / 7) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.2 * window.pageYOffset) + 'px';
+    })
+});
