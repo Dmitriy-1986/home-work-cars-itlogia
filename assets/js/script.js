bf1bb7704cce505b0af5390b1f084571c289c5e6
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-19 09:59:07 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,23 @@ for(const carButton of carButtons) {
         document.querySelector('.price').scrollIntoView({behavior: "smooth"});
     })
 };
+
+
+const priceButton = document.querySelector('#price-button');
+let priceMessage = document.querySelector('#price-message');
+
+priceButton.addEventListener('click', () => {
+    if(document.querySelector('#price-name').value === '') {
+        priceMessage.textContent = 'Заполните поле имя!';
+        priceMessage.classList.add('color-red');
+    } else if(document.querySelector('#price-phone').value === '') {
+        priceMessage.textContent = 'Заполните поле телефона!';
+        priceMessage.classList.add('color-red');
+    } else if(document.querySelector('#price-car').value === '') {
+        priceMessage.textContent = 'Заполните поле автомобиля!';
+        priceMessage.classList.add('color-red');
+    } else {
+        priceMessage.textContent = 'Спасибо за заявку, мы свяжемся с Вами в ближайшее время!';
+        priceMessage.className = 'color-blue';
+    }
+});
